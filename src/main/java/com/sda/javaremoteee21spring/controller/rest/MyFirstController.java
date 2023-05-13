746674312f295f0abfce97bc6858989f455e140a
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-13 12:12:33 +0200

    try-login endpoint with GET http method

--- src/main/java/com/sda/javaremoteee21spring/controller/rest/MyFirstController.java
+++ src/main/java/com/sda/javaremoteee21spring/controller/rest/MyFirstController.java
@@ -1,8 +1,11 @@
 package com.sda.javaremoteee21spring.controller.rest;
 
 import com.sda.javaremoteee21spring.dto.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,6 +14,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MyFirstController {
+
+    // @Slf4j
+    private static final Logger log = LoggerFactory.getLogger(MyFirstController.class);
 
     @GetMapping("/name")
     public String showMeMyName() {
@@ -22,7 +28,8 @@ public class MyFirstController {
         return new Person("mariusz", "p.", 18);
     }
 
-    @GetMapping("me-and-friend")
+    @GetMapping(value = "me-and-friend")
+//    @GetMapping("me-and-friend")
     public List<Person> meAndFriend() {
         List<Person> persons = new ArrayList<>();
         // persons.add(...);
@@ -31,5 +38,14 @@ public class MyFirstController {
                 new Person("mariusz", "p.", 18),
                 new Person("john", "k.", 25)
         );
+    }
+
+    // ?param1=value1&param2=value2
+    // /api/try-login?login=mariusz&password=12345
+    @GetMapping("/try-login")
+    public String logIn(@RequestParam(defaultValue = "John Doe", value = "login") String userLogin,
+                        @RequestParam(defaultValue = "pass", value = "password") String userPassword) {
+        log.info("received login: [{}] and password: [{}]", userLogin, userPassword);
+        return "OK";
     }
 }
