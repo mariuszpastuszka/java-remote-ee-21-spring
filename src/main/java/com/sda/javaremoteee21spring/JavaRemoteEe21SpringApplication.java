fd0dcf4f438a3b075e4f9e4588ec08966266916b
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-13 09:31:08 +0200

    [init] - initial project commit

---
+++ src/main/java/com/sda/javaremoteee21spring/JavaRemoteEe21SpringApplication.java
@@ -0,0 +1,13 @@
+package com.sda.javaremoteee21spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRemoteEe21SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRemoteEe21SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/javaremoteee21spring/JavaRemoteEe21SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.javaremoteee21spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRemoteEe21SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
