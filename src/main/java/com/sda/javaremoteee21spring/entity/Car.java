3b254c981ced44b6a7ebe4df7ac0e6256cd74a1a
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-06-03 11:15:38 +0200

    Improving entity mapping of Car

--- src/main/java/com/sda/javaremoteee21spring/entity/Car.java
+++ src/main/java/com/sda/javaremoteee21spring/entity/Car.java
@@ -1,5 +1,6 @@
 package com.sda.javaremoteee21spring.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,18 +8,32 @@ import lombok.NoArgsConstructor;
 
 import java.time.YearMonth;
 
+@Entity
+@Table(name = "CARS")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder // builder don't need no-args constructor
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     String brand;
+
     String model;
+
     String vin;
+
+    @Enumerated(EnumType.STRING)
     Colour colour;
+
     YearMonth productionYearMonth;
+
     int mileage;
+
+    @Enumerated(EnumType.STRING)
     Fuel fuelType;
+
     boolean insurance;
 }
--- src/main/java/com/sda/javaremoteee21spring/entity/Colour.java
+++ src/main/java/com/sda/javaremoteee21spring/entity/Colour.java
@@ -1,9 +1,26 @@
 package com.sda.javaremoteee21spring.entity;
 
 public enum Colour {
-    SOUL_RED,
-    RED,
-    BlUE,
-    PINK,
-    GRAY
+    SOUL_RED("Nice red"),
+    RED("Normal red"),
+    BlUE("Just blue"),
+    PINK("Pinky"),
+    GRAY("Just gray");
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    Colour(String description) {
+        this.description = description;
+    }
+
+    public static void main(String[] args) {
+        Colour soulRed = SOUL_RED;
+        System.out.println(soulRed.getDescription());
+        System.out.println("name is: " + soulRed.name());
+        System.out.println("Ordinal is: " + soulRed.ordinal());
+    }
 }
