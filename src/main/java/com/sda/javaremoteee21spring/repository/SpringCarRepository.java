1382a345ea4ab996001a637296c4ae5c140a238b
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-06-04 09:37:11 +0200

    Using JpaRepository instead of CrudRepository in SpringCarRepository

--- src/main/java/com/sda/javaremoteee21spring/repository/SpringCarRepository.java
+++ src/main/java/com/sda/javaremoteee21spring/repository/SpringCarRepository.java
@@ -1,7 +1,7 @@
 package com.sda.javaremoteee21spring.repository;
 
 import com.sda.javaremoteee21spring.entity.Car;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpringCarRepository extends CrudRepository<Car, Long> {
+public interface SpringCarRepository extends JpaRepository<Car, Long> {
 }
