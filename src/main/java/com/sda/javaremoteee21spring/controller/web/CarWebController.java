6426141ad14d088324f5c914003757214a68587c
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-06-10 10:58:32 +0200

    Saving cars - in progress

--- src/main/java/com/sda/javaremoteee21spring/controller/web/CarWebController.java
+++ src/main/java/com/sda/javaremoteee21spring/controller/web/CarWebController.java
@@ -1,13 +1,11 @@
 package com.sda.javaremoteee21spring.controller.web;
 
+import com.sda.javaremoteee21spring.entity.Car;
 import com.sda.javaremoteee21spring.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
@@ -52,6 +50,21 @@ public class CarWebController {
 
         carService.deleteCarById(id);
         // redirect creates Location header with final url to go
+        return "redirect:/web/cars";
+    }
+
+    @GetMapping("/cars/show-add-form")
+    public String newCarForm() {
+        log.info("Presenting new car form for user");
+        return "new-car";
+    }
+
+    @PostMapping("/cars/save")
+    public String saveCar(Car carToSave) {
+        log.info("Saving new car: [{}]", carToSave);
+
+        // TODO: implement
+//        carService.saveCar(carToSave);
         return "redirect:/web/cars";
     }
 }
